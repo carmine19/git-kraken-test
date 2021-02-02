5b4923a930ba11fd693f34f1e080bc7fef625229
carmine <carminepepiciello@gmail.com>
2021-02-02 16:12:39 +0100

    fix layouts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Tutti gli utenti</h1>
+                <h1>Tutti i post</h1>
                 <table class="table">
                     <thead>
                         <tr>
